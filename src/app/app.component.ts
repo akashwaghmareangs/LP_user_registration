cd863fcc6409c48e852982dd3248fd3994156177
akash <akashwaghmareangs@gmail.com>
2025-01-17 13:25:05 +0530

    register form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule,AsyncPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+ 
   title = 'LP_User_Register';
+
+  userObj :USER =new USER();
+
+  http=inject(HttpClient)
+  cityList$:Observable<string[]> = new Observable<string[]>();
+
+  ngOnInit(): void {
+  this.cityList$ = this.http.get<string[]>('http://localhost:3000/cityList');
+  }
+  getCity(){
+
+  }
+}
+export class USER {
+  userId: number;
+  username: string;
+  Fname: string;
+  Lname: string;
+  city: string;
+  state: string;
+  zipcode: string;
+
+  constructor() {
+    this.userId = 0;
+    this.username = '';
+    this.Fname = '';
+    this.Lname = '';
+    this.city = '';
+    this.state = '';
+    this.zipcode = '';
+  }
 }
